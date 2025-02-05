096e80581d91010b0dc8e5254e02f7210a5a68dc
elvin4 <elviny.jj@gmail.com>
2025-02-06 00:55:09 +0100

    Add breadcrumbs and remade category page

---
+++ learnit/courses/mixins/breadcrumbs_mixin.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+
+class BreadcrumbsMixin:
+    breadcrumbs = None
+
+    def get_breadcrumbs(self):
+        """
+        Возвращает список крошек.
+        """
+        if self.breadcrumbs is not None:
+            return self.breadcrumbs
+        return [
+            {"name": _("Home"), "url": reverse("main")},
+        ]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["breadcrumbs"] = self.get_breadcrumbs()
+        
+        return context
--- learnit/courses/views.py
+++ learnit/courses/views.py
@@ -1,51 +1,105 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.views import View
+from django.views.generic import TemplateView, ListView
+from django.urls import reverse
 from django.utils.translation import get_language
+from django.db.models import Q
 from .models import Course, Category, Lesson, Language
+from .mixins.breadcrumbs_mixin import BreadcrumbsMixin
 
 
-class MainPage(View):
+class MainPage(TemplateView):
     """Главная страница курсов"""
 
-    def get(self, request):
-        current_language_code = get_language()
-        current_language = Language.objects.get(code=current_language_code)
+    template_name = "courses/base.html"
 
+    def get_context_data(self, **kwargs):
+        # Получаем текущий язык
+        current_language_code = get_language()
+        current_language = get_object_or_404(Language, code=current_language_code)
+
+        # Получаем курсы и категории для текущего языка
         courses = Course.objects.filter(language=current_language)
         categories = Category.objects.filter(language=current_language)
 
-        return render(
-            request,
-            "courses/base.html",
-            context={"courses": courses, "categories": categories},
-        )
 
+        context = super().get_context_data(**kwargs)
 
-class CategoryPage(View):
+        # Добавляем курсы и категории в контекст
+        context.update({
+            "courses": courses,
+            "categories": categories,
+        })
+
+        return context
+
+class CategoryPage(BreadcrumbsMixin, ListView):
     """Вывод всех курсов по категории"""
 
-    def get(self, request, category):
-        category = get_object_or_404(Category.objects.all().filter(slug=category))
-        child_categories = Category.objects.filter(parent_category=category)
-        courses = Course.objects.all().filter(category=category)
+    model = Course
+    template_name = "courses/category.html"
+    context_object_name = "courses"
+    paginate_by = 20  
 
-        return render(
-            request,
-            "courses/category.html",
-            context={
-                "category": category,
-                "courses": courses,
-                "child_categories": child_categories,
-            },
+    def get_queryset(self):
+        category = get_object_or_404(Category, slug=self.kwargs['category'])
+        child_categories = Category.objects.filter(parent_category=category)
+
+
+        queryset = Course.objects.filter(
+            Q(category=category)
+            | Q(category__in=child_categories)
+        ).filter(is_published=True).order_by(
+            "-priority",
+            "-average_views",
+            "-modified_date",
         )
 
+        self.category = category
+        self.child_categories = child_categories
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Добавляем в контекст текущую категорию и дочерние категории
+        context['category'] = self.category
+        context['child_categories'] = self.child_categories
+
+        # Получаем хлебные крошки с учетом текущей категории
+        context['breadcrumbs'] = self.get_breadcrumbs()
+
+        return context
+
+    def get_breadcrumbs(self):
+        # Получаем хлебные крошки из миксина
+        breadcrumbs = super().get_breadcrumbs()
+
+        categories = [self.category]
+        # Добавляем родительские категории (если есть)
+        parent_category = self.category.parent_category
+        while parent_category:
+            categories.insert(0, parent_category)  # Вставляем в начало, чтобы они шли до текущей категории
+            parent_category = parent_category.parent_category
+
+        # Добавляем текущую категорию в хлебные крошки
+        for category in categories:
+            breadcrumbs.append(
+                {
+                    "name": category.name,
+                    "url": reverse("category", kwargs={"category": category.slug}),
+                }
+            )
+        return breadcrumbs
+    
 
 class CoursePage(View):
     """Вывод всех уроков по курсу"""
 
     def get(self, request, course_slug):
-        course = get_object_or_404(Course.objects.filter(slug=course_slug))
+        course = get_object_or_404(Course, slug=course_slug)
         lessons = Lesson.objects.filter(course=course)
 
         return render(
