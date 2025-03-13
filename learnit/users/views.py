47182b9855ca6a232b45a191d859a63c42a44bd5
elvin4 <elviny.jj@gmail.com>
2025-03-13 17:49:35 +0100

    feat: add sign in form

--- learnit/users/views.py
+++ learnit/users/views.py
@@ -1,29 +1,12 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
-from django.urls import reverse
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 from .forms import LoginUserForm
 
 
-def login_user(request):
-    if request.method == "POST":
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(
-                request, username=cd["username"], password=cd["password"]
-            )
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = "users/login.html"
 
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse("home"))
-
-    else:
-        form = LoginUserForm()
-    return render(request, "users/login.html", {"form": form})
-
-
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("users:login"))
+    def get_success_url(self):
+        return reverse_lazy("main")
