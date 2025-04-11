261b5cf3453bfd71417501b5bdfd452373763e20
elvin4 <elviny.jj@gmail.com>
2025-04-12 01:08:19 +0200

    feat: add logout function

--- learnit/users/forms.py
+++ learnit/users/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
+from django.utils.translation import gettext as _
+
 
 class LoginUserForm(AuthenticationForm):
     class Meta:
@@ -10,17 +12,17 @@ class LoginUserForm(AuthenticationForm):
 
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label="Username")
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput())
-    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput())
+    username = forms.CharField(label=_("Username"))
+    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput())
+    password2 = forms.CharField(
+        label=_("Repeat password"), widget=forms.PasswordInput()
+    )
 
     class Meta:
         model = get_user_model()
         fields = [
             "username",
             "email",
-            "first_name",
-            "last_name",
             "password1",
             "password2",
         ]
@@ -33,13 +35,13 @@ class RegisterUserForm(UserCreationForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd["password1"] != cd["password2"]:
-            raise forms.ValidationError("Passwords not sovpadaet")
+            raise forms.ValidationError(_("Passwords do not match"))
 
         return cd["password1"]
 
     def clean_email(self):
         email = self.cleaned_data["email"]
         if get_user_model().objects.filter(email=email).exists():
-            raise forms.ValidationError("E-mail alerady registred")
+            raise forms.ValidationError(_("E-mail alerady registred"))
 
         return email
